d0e78ddd05413fb88f2e35682dbf92fb85b45e01
Stefano Corra <stefano.corra@hotmail.com>
2022-03-19 23:38:41 -0400

    PostWidget commit

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,8 +1,24 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import moment from "moment"
 import Link from "next/link"
 
-const PostWidget = () => {
+import { getRecentPosts } from "../services"
+
+const PostWidget = ({ categories, slug }) => {
+  const [relatedPosts, setrelatedPosts] = useState([])
+
+  useEffect(() => {
+    if (slug) {
+      getSimilarPosts(category, slug).then((result) => setrelatedPosts(results))
+    } else {
+      getRecentPosts(category, slug).then((result) => setrelatedPosts(results))
+    }
+
+    return () => {
+      second
+    }
+  }, [third])
+
   return <div>PostWidget</div>
 }
 
