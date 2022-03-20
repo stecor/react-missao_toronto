6c8146ca302fc910dae0b8c5c15131688a6a7878
Stefano Corra <stefano.corra@hotmail.com>
2022-03-20 16:29:13 -0400

    Categories commit

--- services/index.js
+++ services/index.js
@@ -39,6 +39,42 @@ export const getPosts = async () => {
   return result.postsConnection.edges
 }
 
+export const getPostDetails = async (slug) => {
+  const query = gql`
+    query getPostDetails($slug: String!) {
+      post(where: { slug: $slug }) {
+        author {
+          bio
+          name
+          id
+          photo {
+            url
+          }
+          posts {
+            id
+          }
+        }
+        createdAt
+        slug
+        title
+        excerpt
+        featuredImage {
+          url
+        }
+        categories {
+          name
+          slug
+        }
+        content {
+          raw
+        }
+      }
+    }
+  `
+  const result = await request(graphqlAPI, query, { slug })
+  return result.post
+}
+
 export const getRecentPosts = async () => {
   const query = gql`
   query GetPostDetails(){
@@ -59,7 +95,7 @@ export const getRecentPosts = async () => {
   return result.posts
 }
 
-export const getSimilarPosts = async () => {
+export const getSimilarPosts = async (categories, slug) => {
   const query = gql`
     query GetPostDetails($slug: String!, $categories: [String!]) {
       posts(
@@ -78,7 +114,7 @@ export const getSimilarPosts = async () => {
       }
     }
   `
-  const result = await request(graphqlAPI, query)
+  const result = await request(graphqlAPI, query, { categories, slug })
   return result.posts
 }
 
