e08c3fd89d2962cd6ff939b9d9a039c586ca00be
Stefano Corra <stefano.corra@hotmail.com>
2022-03-20 15:28:42 -0400

    Categories commit

--- services/index.js
+++ services/index.js
@@ -85,7 +85,7 @@ export const getSimilarPosts = async () => {
 export const getCategories = async () => {
   const query = gql`
     query GetCategories {
-      categories:{
+      categories {
         name
         slug
       }
