4c12ba9e2976024f1bcdd2488b6b53bb79f5615d
Stefano Corra <stefano.corra@hotmail.com>
2022-03-21 21:20:13 -0400

    Design commit

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import Link from 'next/link'
-
+import Image from 'next/image'
 import { getCategories } from '../services'
 
 const Header = () => {
@@ -12,14 +12,22 @@ const Header = () => {
 
   return (
     <div className="container mx-auto mb-8 px-10">
-      <div className="inline-block w-full border-b border-blue-400 py-8">
-        <div className="block md:float-left">
+      <div className="inline-block w-full border-b border-white py-8">
+        <div className="flex justify-center  md:float-left">
+          <Image
+            src="/logo.jpg"
+            alt="Igreja Missão Toronto"
+            width="80px"
+            height="80px"
+            className="absolute h-80 w-full rounded-t-lg object-cover object-top  shadow-lg lg:rounded-lg"
+          />
           <Link href="/">
-            <span className="cursor-pointer text-4xl font-bold text-white">
+            <span className="mt-5 ml-7 cursor-pointer text-5xl font-bold text-white">
               Igreja Missão Toronto
             </span>
           </Link>
         </div>
+
         <div className="hidden md:float-left md:contents">
           {categories.map((category) => (
             <Link key={category.slug} href={`/category/${category.slug}`}>
--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,7 +1,7 @@
-import React from "react"
-import moment from "moment"
-import Link from "next/link"
-import Image from "next/image"
+import React from 'react'
+import moment from 'moment'
+import Link from 'next/link'
+import Image from 'next/image'
 
 const PostCard = ({ post }) => {
   console.log(post)
@@ -14,7 +14,7 @@ const PostCard = ({ post }) => {
           className="absolute h-80 w-full rounded-t-lg object-cover object-top shadow-lg lg:rounded-lg"
         />
       </div>
-      <h1 className="mb-8 cursor-pointer text-center text-3xl font-semibold transition duration-700 hover:text-pink-600">
+      <h1 className="mb-8 cursor-pointer text-center text-3xl font-semibold transition duration-700 hover:text-purple-600">
         <Link href={`/post/${post.slug}`}>{post.title}</Link>
       </h1>
       <div className="mb-8 block w-full items-center justify-center text-center lg:flex">
@@ -34,7 +34,7 @@ const PostCard = ({ post }) => {
         <div className="font-medium text-gray-700">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            className="mr-2 inline h-6 w-6 text-pink-500"
+            className="mr-2 inline h-6 w-6 text-purple-500"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
@@ -46,7 +46,7 @@ const PostCard = ({ post }) => {
               d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
             />
           </svg>
-          <span>{moment(post.createdAt).format("MMM DD, YYYY")}</span>
+          <span>{moment(post.createdAt).format('MMM DD, YYYY')}</span>
         </div>
       </div>
       <p className="mb-8 px-4 text-center text-lg font-normal text-gray-700 lg:px-20">
@@ -54,7 +54,7 @@ const PostCard = ({ post }) => {
       </p>
       <div className="text-center">
         <Link href={`/post/${post.slug}`}>
-          <span className="inline-block transform cursor-pointer rounded-full bg-pink-600 px-8 py-3 text-lg font-medium text-white transition duration-500 hover:-translate-y-1">
+          <span className="inline-block transform cursor-pointer rounded-full bg-gray-800 px-8 py-3 text-lg font-medium text-white transition duration-500 hover:-translate-y-1">
             Continue Reading
           </span>
         </Link>
