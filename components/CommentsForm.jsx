48645ea3f6fc9b7483d5c1bc9bb68d78809473b2
Stefano Corra <stefano.corra@hotmail.com>
2022-03-26 11:15:16 -0400

    CommentsForm commit

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -8,6 +8,8 @@ const CommentsForm = ({ slug }) => {
   const nameEl = useRef()
   const emailEl = useRef()
   const storeDataEl = useRef()
+
+  const handleCommentSubmission = () => {}
   return (
     <div className="rounded-lg bg-white p-8 pb-12 shadow-lg">
       <h3 className="mb-8 border-b pb-4 text-xl font-semibold">CommentsForm</h3>
@@ -19,9 +21,40 @@ const CommentsForm = ({ slug }) => {
           name="comment"
         />
       </div>
-      <div className="mb-4 grid grid-cols-1 gap-4"></div>
-      <div className="mb-4 grid grid-cols-1 gap-4"></div>
-      <div className="mb-4 grid grid-cols-1 gap-4"></div>
+      <div className="mb-4 grid grid-cols-2 gap-4">
+        <input
+          type="text"
+          ref={nameEl}
+          className="w-full rounded-lg bg-gray-100 py-2 px-4 text-gray-700 outline-none focus:ring-2 focus:ring-gray-200 lg:grid-cols-2"
+          placeholder="Name"
+          name="name"
+        />
+
+        <input
+          type="text"
+          ref={emailEl}
+          className="w-full rounded-lg bg-gray-100 py-2 px-4 text-gray-700 outline-none focus:ring-2 focus:ring-gray-200"
+          placeholder="Email"
+          name="email"
+        />
+      </div>
+      {error && (
+        <p className="text-xs text-red-500">All fields are required.</p>
+      )}
+      <div className="mt-8">
+        <button
+          type="button"
+          onClick={handleCommentSubmission}
+          className="ease  inline-block cursor-pointer rounded-full bg-purple-600 px-8 py-3 text-lg text-white transition duration-500 hover:bg-indigo-900"
+        >
+          Post Comment
+        </button>
+        {showSuccessMessage && (
+          <span className="float-right mt-3 text-xl font-semibold text-green-500">
+            Comment Submitted for review.
+          </span>
+        )}
+      </div>
     </div>
   )
 }
