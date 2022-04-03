5631a5fbf17cce7d80b457c8ca8d2de8073ddd45
Stefano Corra <stefano.corra@hotmail.com>
2022-04-02 21:34:31 -0400

    category commit

--- services/index.js
+++ services/index.js
@@ -180,3 +180,75 @@ export const getFeaturedPosts = async () => {
 
   return result.posts
 }
+
+export const getAdjacentPosts = async (createdAt, slug) => {
+  const query = gql`
+    query GetAdjacentPosts($createdAt: DateTime!, $slug: String!) {
+      next: posts(
+        first: 1
+        orderBy: createdAt_ASC
+        where: { slug_not: $slug, AND: { createdAt_gte: $createdAt } }
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+      previous: posts(
+        first: 1
+        orderBy: createdAt_DESC
+        where: { slug_not: $slug, AND: { createdAt_lte: $createdAt } }
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `
+
+  const result = await request(graphqlAPI, query, { slug, createdAt })
+
+  return { next: result.next[0], previous: result.previous[0] }
+}
+
+export const getCategoryPost = async (slug) => {
+  const query = gql`
+    query GetCategoryPost($slug: String!) {
+      postsConnection(where: { categories_some: { slug: $slug } }) {
+        edges {
+          cursor
+          node {
+            author {
+              bio
+              name
+              id
+              photo {
+                url
+              }
+            }
+            createdAt
+            slug
+            title
+            excerpt
+            featuredImage {
+              url
+            }
+            categories {
+              name
+              slug
+            }
+          }
+        }
+      }
+    }
+  `
+
+  const result = await request(graphqlAPI, query, { slug })
+
+  return result.postsConnection.edges
+}
