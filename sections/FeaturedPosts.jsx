48aaf6b1d6b8a81f114dc5c369295a52408af2ac
Stefano Corra <stefano.corra@hotmail.com>
2022-04-03 20:09:29 -0400

    category commit

--- sections/FeaturedPosts.jsx
+++ sections/FeaturedPosts.jsx
@@ -58,7 +58,7 @@ const FeaturedPosts = () => {
     <div className="arrow-btn absolute right-0 cursor-pointer rounded-full bg-pink-600 py-3 text-center">
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        className="h-6 w-6 w-full text-white"
+        className="h-6  w-full text-white"
         fill="none"
         viewBox="0 0 24 24"
         stroke="currentColor"
